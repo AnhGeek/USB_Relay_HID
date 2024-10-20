9770542df0b9c4ddbd59583f31a02c17b3c44c6e
HoangAnh <lyhoanganh.hcm05@gmail.com>
2024-10-20 14:42:06 +0700

    reach to stage get configuration

--- main.c
+++ main.c
@@ -22,6 +22,28 @@ SBIT(P3_1,0xB0,1);
 
 __xdata uint8_t u8Buff[64];
 
+uint8_t u8UsbIndex;
+const uint8_t u8DeviceDescriptor[] = {
+	0x12, /* 0 */
+	0x01, /* 1 */
+	0x00, /* 2 */
+	0x02, /* 3 */
+	0xff, /* 4 */
+	0x00, /* 5 */
+	0x00, /* 6 */
+	0x40, /* 7 */
+	0x34, /* 8 */
+	0x12, /* 9 */
+	0x78, /* 10 */
+	0x56, /* 11 */
+	0x00, /* 12 */
+	0x02, /* 13 */
+	0x00, /* 14 */
+	0x00, /* 15 */
+	0x00, /* 16 */
+	0x01 /* 17 */
+};
+
 void send(uint8_t u8Data);
 
 void send(uint8_t u8Data)
@@ -65,28 +87,132 @@ void main(void)
 	T2MOD |= (1 << 4);
 	TMOD = 0x01;
 	
-	USB_CTRL = 0b01101001; // Low speed
-	UDEV_CTRL |= (1 << 2) | (1 << 0);
-	
-	while (1) {
-		if (UIF_BUS_RST) {
+	if(0){
+		USB_CTRL = 0b01101001; // Low speed
+		UDEV_CTRL |= (1 << 2) | (1 << 0);
+	} else {
+		// Highspeed
+		USB_CTRL = (1 << 5) | (1 << 3) | (1 << 0);
+		UDEV_CTRL |= (1 << 0); 
+	}
+	u8UsbIndex = 0;
+	while (1) 
+	{
+		if (UIF_BUS_RST) 
+		{
 			P3_1 = 1;
 			P3_1 = 0;
 			UEP0_DMA = (uint16_t)u8Buff;
 			UEP0_CTRL = 0x02;
 			UIF_BUS_RST = 0;
 		}
+		
 		if (UIF_TRANSFER) {
-			P3_1 = 1;
-			P3_1 = 0;
-			P3_1 = 1;
-			P3_1 = 0;
-			u8Buff[0] = 0x14;
-			u8Buff[1] = 0x12;
-			u8Buff[2] = 0x19;
-			u8Buff[3] = 0x86;
-			UEP0_T_LEN = 4;
-			UEP0_CTRL = 0x00;
+			/* Program will fall in to this handler after a transfer being successful
+			   You must prepare data within the previous transaction. */
+			if ((USB_INT_ST & 0x0F) == 0x00) {
+				/* reset buffer */
+				UEP0_T_LEN = 0;
+				// New request to EP0?
+				switch((USB_INT_ST & 0x30)){
+					case 0x30:
+						//Setup transfer
+						P3_1 = 1;
+						P3_1 = 0;
+						if (u8Buff[0] & 0x80) {
+							/* Check bmRequestType = device to host */
+							
+							switch (u8Buff[1]) {
+								case 0x06:
+									/* bRequest == Get descriptor */
+									switch (u8Buff[3]) {
+										//Check descriptor types
+										case 0x01:
+											P3_1 = 1;
+											P3_1 = 0;
+											P3_1 = 1;
+											P3_1 = 0;
+											P3_1 = 1;
+											P3_1 = 0;
+											/* device descriptor */
+											if (u8Buff[6] >= 0x12) {
+												for (i = 0; i < 0x12; ++i) {
+													u8Buff[i] = u8DeviceDescriptor[i];
+												}
+												UEP0_T_LEN = 0x12;
+												UEP0_CTRL = 0x80 | 0x40;
+											}
+											break;
+									}
+									break;
+								default:
+									//Default
+									break;
+							}
+						} 
+						else 
+						{
+							/* Check bmRequestType = host to device */
+							P3_1 = 1;
+							P3_1 = 0;
+							P3_1 = 1;
+							P3_1 = 0;
+							P3_1 = 1;
+							P3_1 = 0;
+							P3_1 = 1;
+							P3_1 = 0;
+							P3_1 = 1;
+							P3_1 = 0;
+							switch (u8Buff[1]) {
+								case 0x05:
+									/* bRequest == SET_ADDRESS */
+									u8UsbIndex = u8Buff[2];
+									UEP0_T_LEN = 0;
+									UEP0_CTRL = 0x80 | 0x40;
+									break;
+								default:
+									//Default
+									break;
+							}
+						}
+						break;	
+					case 0x20:
+						//In packets
+						P3_1 = 1;
+						P3_1 = 0;
+						P3_1 = 1;
+						P3_1 = 0;
+						P3_1 = 1;
+						P3_1 = 0;
+						P3_1 = 1;
+						P3_1 = 0;
+						if ((u8UsbIndex != 0x00) && (USB_DEV_AD != u8UsbIndex )) 
+						{
+							USB_DEV_AD = u8UsbIndex;
+						}
+						break;
+						
+					case 0x00:
+						//Out packets
+						P3_1 = 1;
+						P3_1 = 0;
+						P3_1 = 1;
+						P3_1 = 0;
+						P3_1 = 1;
+						P3_1 = 0;
+						P3_1 = 1;
+						P3_1 = 0;
+						P3_1 = 1;
+						P3_1 = 0;
+						P3_1 = 1;
+						P3_1 = 0;
+						break;
+						
+					default:
+						//SOF packets
+						break;
+				}
+			} 
 			UIF_TRANSFER = 0;
 		}
 	}
