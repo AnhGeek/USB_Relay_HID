64895504b1b9e27823965399f3f711ec9e9c10c2
HoangAnh <lyhoanganh.hcm05@gmail.com>
2024-10-27 00:55:19 +0700

    add usb config files

---
+++ UsbDescriptor.c
@@ -0,0 +1,39 @@
+#include "UsbDef.h"
+#include <stdint.h>
+#include "UsbDescriptor.h"
+
+/* device descriptor */
+static uint8_t _DevDesc[] = 
+{
+	0x12,                 /*bLength */
+	USB_DESCR_TYP_DEVICE, /*bDescriptorType*/
+#if USB_DEVICE_TYPE == LOW_SPEED_DEVICE
+	0x10, 0x01,           /*bcdUSB */
+#elif USB_DEVICE_TYPE == FULL_SPEED_DEVICE
+	0x00, 0x02,           /*bcdUSB */
+#else
+	#error "usb device type error"
+#endif
+	0x00,                 /*bDeviceClass*/
+	0x00,                 /*bDeviceSubClass*/
+	0x00,                 /*bDeviceProtocol*/
+	DEFAULT_ENDP0_SIZE,   /*bMaxPacketSize*/
+	0x3b, 0x41,           /*idVendor (0x413b)*/
+	0x07, 0x21,           /*idProduct = 0x2107*/
+	0x00, 0x00,           /*bcdDevice rel. 0.00*/
+	0x00,                 /*Index of string descriptor describing
+                                                  manufacturer */
+	0x00,                 /*Index of string descriptor describing
+                                                 product*/
+    0x00,                 /*Index of string descriptor describing the
+                                                 device serial number */
+    0x01                  /*bNumConfigurations*/
+};
+
+/* device descriptor */
+const DescriptorData DevDesc = 
+{
+    _DevDesc,
+
+    sizeof(_DevDesc)
+};
---
+++ UsbDescriptor.h
@@ -0,0 +1,23 @@
+#define USB_DESCR_TYP_DEVICE    0x01
+#define USB_DESCR_TYP_CONFIG    0x02
+#define USB_DESCR_TYP_STRING    0x03
+#define USB_DESCR_TYP_INTERF    0x04
+#define USB_DESCR_TYP_ENDP      0x05
+#define USB_DESCR_TYP_QUALIF    0x06
+#define USB_DESCR_TYP_SPEED     0x07
+#define USB_DESCR_TYP_OTG       0x09
+#define USB_DESCR_TYP_BOS       0x0f
+#define USB_DESCR_TYP_HID       0x21
+#define USB_DESCR_TYP_REPORT    0x22
+#define USB_DESCR_TYP_PHYSIC    0x23
+#define USB_DESCR_TYP_CS_INTF   0x24
+#define USB_DESCR_TYP_CS_ENDP   0x25
+#define USB_DESCR_TYP_HUB       0x29
+
+#define LOW_SPEED_DEVICE        0
+#define FULL_SPEED_DEVICE       1
+	
+#define USB_DEVICE_TYPE         LOW_SPEED_DEVICE
+
+/* device descriptor */
+extern const DescriptorData DevDesc;
